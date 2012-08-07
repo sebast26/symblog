ec51becdb6dbfca605537e4c3cd2522ee422a2b5
Sebastian Gorecki <gorecki.sebastian@gmail.com>
2012-08-07 23:04:10 +0200

    dodano formularz kontaktowy oraz wysylanie maili do admina

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -3,6 +3,8 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -14,5 +16,37 @@ class PageController extends Controller
 	public function aboutAction()
 	{
 		return $this->render('BloggerBlogBundle:Page:about.html.twig');
+	}
+
+	public function contactAction()
+	{
+		$enquiry = new Enquiry();
+		$form = $this->createForm(new EnquiryType(), $enquiry);
+
+		$request = $this->getRequest();
+		if ($request->getMethod() == 'POST') {
+			$form->bindRequest($request);
+
+			if ($form->isValid()) {
+				// Perform some action, such as sending an email
+				$message = \Swift_Message::newInstance()
+					->setSubject('Contact enquiry from symblog')
+					->setFrom('enquiries@symblog.co.uk')
+					->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+					->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array(
+						'enquiry' => $enquiry)));
+				$this->get('mailer')->send($message);
+
+				$this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+				// Redirect - This is important to prevent users re-posting
+				// the form if they refresh the page
+				return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+			}
+		}
+
+		return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+			'form' => $form->createView()
+		));
 	}
 }
